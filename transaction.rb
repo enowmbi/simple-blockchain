1d2ed494660b98a274408de12e526093cd3e7b35
Enow Mbi <benowmbi@yahoo.com>
2020-03-24 16:27:30 +0100

    Modify transaction -- add getters using attr_reader for address_of_sender,address_of_receiver and amount

--- transaction.rb
+++ transaction.rb
@@ -1,4 +1,5 @@
 class Transaction
+  attr_reader :address_of_sender, :address_of_receiver,:amount
   def initialize(address_of_sender = '',address_of_receiver,amount)
       @address_of_sender = address_of_sender
       @address_of_receiver = address_of_receiver
