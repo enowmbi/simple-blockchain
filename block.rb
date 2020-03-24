61188d62743b8aa510623c88494d9170d14a35fe
Enow Mbi <benowmbi@yahoo.com>
2020-03-24 12:54:22 +0100

    Create block with initialize and calculate_hash methods

---
+++ block.rb
@@ -0,0 +1,19 @@
+class Block
+  
+  require 'digest'
+
+  attr_reader :data
+  attr_accessor :previous_hash, :hash
+
+  def initialize(timestamp, transaction)
+    @timestamp = timestamp
+    @transaction = transaction
+  end
+
+  def calculate_hash()
+    payload = @timestamp.to_s + @data.to_s + @previous_hash.to_s
+    Digest::SHA256.hexdigest(payload)
+  end
+
+end
+
