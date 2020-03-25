a8652d25d713ac9967ab7669f0882b3d71ff4c3f
Enow Mbi <benowmbi@yahoo.com>
2020-03-25 05:33:37 +0100

    Modify blockchain.rb --add getter for pending_transactions, initialize pending_transactions to empty array,change address_of_sender,address_of_receiver to nil from empty string when creating genesisblock and when mining a block, add mining_pending_transaction method, add create_pending_transaction_to_blockchain method, remove unnecessary comments

--- blockchain.rb
+++ blockchain.rb
@@ -3,29 +3,48 @@ class Blockchain
   require_relative 'block'
   require_relative 'transaction'
 
-  attr_reader :chain, :difficulty
+  attr_reader :chain, :difficulty, :pending_transactions
 
   def initialize
     @chain = []
     @difficulty = 2
+    @pending_transactions =[]
+    @mining_reward = 100
     #initialize with genesis block
     create_genesis_block
   end
 
   def create_genesis_block
     #create a new block with manually set values and then push it to the chain
-    genesis_block = Block.new(Time.now(),Transaction.new("","",0))   
+    genesis_block = Block.new(Time.now(),Transaction.new(nil,nil,0))   
     genesis_block.previous_hash ='0'
     genesis_block.hash = genesis_block.calculate_hash()
     @chain << genesis_block
   end
 
+  def mine_pending_transactions(mining_reward_address)
+    new_block = Block.new(Time.now(),@pending_transactions.first)
+    new_block.previous_hash = get_lattest_block.hash
+    new_block.mine_block(@difficulty)
+    @chain << new_block
+    #remove the processed transaction the the block
+    @pending_transactions.shift
+    # add transaction on the blockchain to reward miner
+    create_pending_transaction_on_blockchain(Transaction.new(nil,mining_reward_address,@mining_reward))
+  end
+
+  def create_pending_transaction_on_blockchain(transaction)
+    @pending_transactions << transaction
+  end
+
+=begin
   def add_block(block)
     #get the hash of the previous block and also compute the hash of the current block
     block.previous_hash = get_lattest_block.hash
     block.mine_block(@difficulty)
     @chain << block
   end
+=end
 
 
   def get_lattest_block
@@ -33,7 +52,6 @@ class Blockchain
   end
 
   def valid?
-    #iterate through the blockchain and
     return true if @chain.length == 1
     @chain.each_with_index do |block,index|
       begin
@@ -49,17 +67,17 @@ class Blockchain
     end
   end
 
-  def calculate_balance(address_of_wallet)
+  def calculate_balance(wallet_address)
     total_coins_received = 0
     total_coins_sent = 0
     @chain.each do |block|
-      if block.transaction.address_of_receiver == address_of_wallet
+      if block.transaction.address_of_receiver == wallet_address
         total_coins_received += block.transaction.amount
-      elsif block.transaction.address_of_sender == address_of_wallet
+      elsif block.transaction.address_of_sender == wallet_address
         total_coins_sent += block.transaction.amount
       end
     end
-    total = total_entries - total_exits
+    total = total_coins_received - total_coins_sent
     return total
   end
 end
