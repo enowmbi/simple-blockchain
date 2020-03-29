8bf574ed913732aa9a9f2aed32830ca99db0f853
Enow Mbi <benowmbi@yahoo.com>
2020-03-29 14:39:30 +0100

    Modify init.rb --remove generation of keys

--- init.rb
+++ init.rb
@@ -8,72 +8,30 @@ puts "Blockchain initialized with a genesis block... "
 #get the number of blocks on the chain
 puts "Number of blocks on the chain: #{blockchain.chain.length}"
 
-#create signing keys
-ec = OpenSSL::PKey::EC.new('secp256k1')
-puts "Elliptic curve created ..."
-key= ec.generate_key!
-puts "Generated keys - private: #{key.private_key} and public keys: #{key.public_key}"
-wallet_address = key.public_key
-#create a new transaction
-transaction1 = Transaction.new(key.public_key,key.public_key,20)
-signature = transaction1.sign_transaction(key)
-puts "signature of transaction: #{signature}"
-
-#check the validity of the blockchain
-puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
-#add the transaction to pending transaction
-blockchain.add_transaction_to_pending_transactions(transaction1)
-#mine the pending transactions
-blockchain.mine_pending_transactions(wallet_address)
-
-#get the number of blocks on the chain
-puts "Number of blocks on the chain: #{blockchain.chain.length}"
-
-#create a new transaction
-transaction = Transaction.new('Enow','Oben',20)
-blockchain.add_transaction_to_pending_transactions(transaction)
-
 #check the validity of the blockchain
 puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
 
-#add the transaction to pending transaction
-blockchain.add_transaction_to_pending_transactions(transaction)
+1.upto(10) do 
+  #create a new transaction
+  transaction = Transaction.new('Enow','Oben',20)
+  blockchain.add_transaction_to_pending_transactions(transaction)
 
-#mine the pending transactions
-blockchain.mine_pending_transactions('xavier')
+  #add the transaction to pending transaction
+  blockchain.add_transaction_to_pending_transactions(transaction)
 
-#check the validity of the blockchain
-puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
+  #mine the pending transactions
+  blockchain.mine_pending_transactions('xavier')
 
+  #check the validity of the blockchain
+  puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
+end
 #check the balance 
-puts "Your balance is #{blockchain.calculate_balance(wallet_address)}"
-puts "Enow's balance is #{blockchain.calculate_balance('Enow')}"
-puts "Oben's balance is #{blockchain.calculate_balance('Oben')}"
-puts "Xavier's balance is #{blockchain.calculate_balance('Xavier')}"
-
-
-
-#create a new transaction
-transaction = Transaction.new('Aspen','Enow',20)
-blockchain.add_transaction_to_pending_transactions(transaction)
-
-#check the validity of the blockchain
-puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
-
-#add the transaction to pending transaction
-blockchain.add_transaction_to_pending_transactions(transaction)
-
-#mine the pending transactions
-blockchain.mine_pending_transactions('raider miner')
-
-#check the validity of the blockchain
-puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
-
-#check the balance 
-puts "Enow's balance is #{blockchain.calculate_balance('Enow')}"
-puts "Oben's balance is #{blockchain.calculate_balance('Oben')}"
-puts "Xavier's balance is #{blockchain.calculate_balance('Xavier')}"
-
-puts "Aspen's balance is #{blockchain.calculate_balance('Aspen')}"
-
-puts "Oben's balance is #{blockchain.calculate_balance('raid miner')}"
+puts "=================================================="
+puts "Your balance is #{blockchain.get_balance('Aspen')}"
+puts "=================================================="
+puts "Enow's balance is #{blockchain.get_balance('Enow')}"
+puts "=================================================="
+puts "Oben's balance is #{blockchain.get_balance('Oben')}"
+puts "=================================================="
+puts "Xavier's balance is #{blockchain.get_balance('Xavier')}"
+puts "=================================================="
