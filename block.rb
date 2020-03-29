3a23c042d7fb5184233bd6fea1c0ba5e02ea80d0
Enow Mbi <benowmbi@yahoo.com>
2020-03-29 14:15:45 +0100

    Modify block.rb --remove add_block and add add_transaction_to_pending_transactions, -- changed attribute to transactions

--- block.rb
+++ block.rb
@@ -2,16 +2,16 @@ class Block
 
   require 'digest'
 
-  attr_accessor :previous_hash, :hash, :transaction
+  attr_accessor :previous_hash, :hash, :transactions
 
-  def initialize(timestamp, transaction)
+  def initialize(timestamp, transactions)
     @timestamp = timestamp
-    @transaction = transaction
+    @transactions = transactions
     @nonce = 0
   end
 
   def calculate_hash()
-    payload = @timestamp.to_s + @data.to_s + @previous_hash.to_s +  @nonce.to_s
+    payload = @timestamp.to_s + @transactions.to_s + @previous_hash.to_s +  @nonce.to_s
     Digest::SHA256.hexdigest(payload)
   end
 
@@ -19,9 +19,9 @@ class Block
     difficulty_string = '0' * difficulty
     @nonce = 0
     loop do 
-      @hash = calculate_hash()
-      if @hash.start_with?(difficulty_string) 
-        puts "Block mined: #{@hash}"
+      hash = calculate_hash()
+      if hash.start_with?(difficulty_string) 
+        puts "Block mined: #{hash}"
         break
       else
         @nonce +=1
