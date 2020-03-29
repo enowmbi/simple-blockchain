c17a60fed930e3894e79dd4ed92c84c8816a0bd3
Enow Mbi <benowmbi@yahoo.com>
2020-03-29 14:23:39 +0100

    Modify blockchain

--- blockchain.rb
+++ blockchain.rb
@@ -19,45 +19,50 @@ class Blockchain
     genesis_block = Block.new(Time.now(),Transaction.new(nil,nil,0))   
     genesis_block.previous_hash ='0'
     genesis_block.hash = genesis_block.calculate_hash()
-    @chain << genesis_block
+    self.chain << genesis_block
   end
 
   def mine_pending_transactions(mining_reward_address)
-    new_block = Block.new(Time.now(),@pending_transactions.first)
+    temp_holding_for_pending_transactions = []
+    self.pending_transactions.each do |trans|
+      temp_holding_for_pending_transactions << trans
+    end
+    self.pending_transactions.clear
+
+    puts "these are the transactions that are pending: #{temp_holding_for_pending_transactions}"
+    puts "the original pending_transaction: #{self.pending_transactions}"
+
+    new_block = Block.new(Time.now(),temp_holding_for_pending_transactions)
+    puts "this is the new block #{new_block.inspect}"
     new_block.previous_hash = get_lattest_block.hash
-    new_block.mine_block(@difficulty)
-    @chain << new_block
-    #remove the processed transaction the the block
-    @pending_transactions.shift
+    new_block.mine_block(self.difficulty)
+    self.chain << new_block
+
     # add transaction on the blockchain to reward miner
-    create_pending_transaction_on_blockchain(Transaction.new(nil,mining_reward_address,@mining_reward))
+    add_transaction_to_pending_transactions(Transaction.new(nil,mining_reward_address,@mining_reward))
+
+    #remove the processed transaction the the block
+    # self.pending_transactions.clear
   end
 
-  def create_pending_transaction_on_blockchain(transaction)
-    @pending_transactions << transaction
+  def add_transaction_to_pending_transactions(transaction)
+    # return "Address of receiver and Address of sender must not be empty" if !transaction.address_of_sender || !transaction.address_of_receiver
+    # return "Can not add invalid transaction" if !transaction.valid?
+    self.pending_transactions << transaction
   end
-
-=begin
-  def add_block(block)
-    #get the hash of the previous block and also compute the hash of the current block
-    block.previous_hash = get_lattest_block.hash
-    block.mine_block(@difficulty)
-    @chain << block
-  end
-=end
-
 
   def get_lattest_block
-    return @chain[@chain.length - 1] 
+    return self.chain[self.chain.length - 1] 
   end
 
   def valid?
-    return true if @chain.length == 1
-    @chain.each_with_index do |block,index|
+    return true if self.chain.length == 1
+    self.chain.each_with_index do |block,index|
       begin
         new_index = index + 1
-        current_block = @chain[new_index]
-        previous_block = @chain[new_index - 1]
+        current_block = self.chain[new_index]
+        previous_block = self.chain[new_index - 1]
+        return false if !current_block.has_valid_transactions?
         return false if current_block.hash != current_block.calculate_hash()
         return false if current_block.previous_hash != previous_block.hash
       rescue
@@ -68,16 +73,32 @@ class Blockchain
   end
 
   def calculate_balance(wallet_address)
-    total_coins_received = 0
-    total_coins_sent = 0
-    @chain.each do |block|
-      if block.transaction.address_of_receiver == wallet_address
-        total_coins_received += block.transaction.amount
-      elsif block.transaction.address_of_sender == wallet_address
-        total_coins_sent += block.transaction.amount
+    balance = 0
+    coins_received =0
+    coins_sent =0
+    if self.chain.length > 1
+      index_of_last_mined_block_on_chain = self.chain.length - 1
+      index_of_first_mined_block_on_chain = 1
+
+      index_of_first_mined_block_on_chain.upto(index_of_last_mined_block_on_chain) do |index_of_block|
+        block = self.chain[index_of_block]
+        index_of_last_transaction_on_block = block.transactions.count - 1
+        0.upto(index_of_last_transaction_on_block) do |index_of_transaction|
+          transaction = block.transactions[index_of_transaction]
+          if transaction.address_of_receiver == wallet_address
+            coins_received += transaction.amount
+          end
+          if transaction.address_of_sender == wallet_address
+            coins_sent += transaction.amount
+          end
+        end
+      balance = coins_received - coins_sent
+      return balance
       end
+    else
+      return 0
     end
-    total = total_coins_received - total_coins_sent
-    return total
+
   end
+
 end
--- init.rb
+++ init.rb
@@ -26,11 +26,54 @@ blockchain.add_transaction_to_pending_transactions(transaction1)
 #mine the pending transactions
 blockchain.mine_pending_transactions(wallet_address)
 
+#get the number of blocks on the chain
+puts "Number of blocks on the chain: #{blockchain.chain.length}"
+
+#create a new transaction
+transaction = Transaction.new('Enow','Oben',20)
+blockchain.add_transaction_to_pending_transactions(transaction)
+
+#check the validity of the blockchain
+puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
+
+#add the transaction to pending transaction
+blockchain.add_transaction_to_pending_transactions(transaction)
+
+#mine the pending transactions
+blockchain.mine_pending_transactions('xavier')
+
 #check the validity of the blockchain
 puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
 
 #check the balance 
 puts "Your balance is #{blockchain.calculate_balance(wallet_address)}"
+puts "Enow's balance is #{blockchain.calculate_balance('Enow')}"
+puts "Oben's balance is #{blockchain.calculate_balance('Oben')}"
+puts "Xavier's balance is #{blockchain.calculate_balance('Xavier')}"
 
 
 
+#create a new transaction
+transaction = Transaction.new('Aspen','Enow',20)
+blockchain.add_transaction_to_pending_transactions(transaction)
+
+#check the validity of the blockchain
+puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
+
+#add the transaction to pending transaction
+blockchain.add_transaction_to_pending_transactions(transaction)
+
+#mine the pending transactions
+blockchain.mine_pending_transactions('raider miner')
+
+#check the validity of the blockchain
+puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
+
+#check the balance 
+puts "Enow's balance is #{blockchain.calculate_balance('Enow')}"
+puts "Oben's balance is #{blockchain.calculate_balance('Oben')}"
+puts "Xavier's balance is #{blockchain.calculate_balance('Xavier')}"
+
+puts "Aspen's balance is #{blockchain.calculate_balance('Aspen')}"
+
+puts "Oben's balance is #{blockchain.calculate_balance('raid miner')}"
