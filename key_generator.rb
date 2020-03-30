1cae11b526bd951e595b2f495ef4d1f7c039aab3
Enow Mbi <benowmbi@yahoo.com>
2020-03-30 19:51:45 +0100

    Modify key_generator.rb --return hex of private and public keys

--- key_generator.rb
+++ key_generator.rb
@@ -5,10 +5,11 @@ class KeyGenerator
   attr_reader :key_pair, :private_key, :public_key
 
   def initialize
-    ec = OpenSSL::PKey::EC.new('secp256k1')
-    @key_pair = ec.generate_key!
+    elliptic_curve = OpenSSL::PKey::EC.new('secp256k1')
+    @key_pair = elliptic_curve.generate_key
     puts "generated key: #{self.key_pair}"
-    @private_key = self.key_pair.private_key
-    @public_key = self.key_pair.public_key
+    #store hex values of private and public keys
+    @private_key = self.key_pair.private_key.to_s(16)
+    @public_key = self.key_pair.public_key.to_bn.to_s(16)
   end
 end
