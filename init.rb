3186705225eeffcaff351f52a8c8b2b6096f60ab
Enow Mbi <benowmbi@yahoo.com>
2021-10-20 22:59:54 +0100

    Refactor init module for manual testing

--- init.rb
+++ init.rb
@@ -1,31 +1,31 @@
-#require blockchain.rb -- has all references to the libraries
+# require blockchain.rb -- has all references to the libraries
 
 require_relative 'blockchain'
 
-#create an instance of the blockchain with genesis block
+# create an instance of the blockchain with genesis block
 blockchain = Blockchain.new
 puts "Blockchain initialized with a genesis block... "
-#get the number of blocks on the chain
+# get the number of blocks on the chain
 puts "Number of blocks on the chain: #{blockchain.chain.length}"
 
-#check the validity of the blockchain
-puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
+# check the validity of the blockchain
+puts "blockchain is: #{blockchain.valid? ? 'valid' : 'invalid'}"
 
-1.upto(10) do 
-  #create a new transaction
-  transaction = Transaction.new('Enow','Oben',20)
+1.upto(10) do
+  # create a new transaction
+  transaction = Transaction.new('Enow', 'Oben', 20)
   blockchain.add_transaction_to_pending_transactions(transaction)
 
-  #add the transaction to pending transaction
+  # add the transaction to pending transaction
   blockchain.add_transaction_to_pending_transactions(transaction)
 
-  #mine the pending transactions
+  # mine the pending transactions
   blockchain.mine_pending_transactions('xavier')
 
-  #check the validity of the blockchain
-  puts "blockchain is: #{blockchain.valid? ? 'valid':'invalid'}" 
+  # check the validity of the blockchain
+  puts "blockchain is: #{blockchain.valid? ? 'valid' : 'invalid'}"
 end
-#check the balance 
+# check the balance
 puts "=================================================="
 puts "Your balance is #{blockchain.get_balance('Aspen')}"
 puts "=================================================="
